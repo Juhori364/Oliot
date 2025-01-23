c0aa3b0af1f05a03d5b65158b7ce740a01f7c525
Juhori364 <163916599+Juhori364@users.noreply.github.com>
2025-01-23 20:35:44 +0200

    Create Viikko3Chef.cpp

---
+++ Viikko3Chef.cpp
@@ -0,0 +1,31 @@
+#include "chef.h"
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+Chef::Chef(){
+    chefName="testi";
+};
+Chef::~Chef(){
+    cout<<"Chef "<<chefName<< " tuhottu"<<"\n";
+};
+
+Chef::Chef(string n) {
+    chefName=n;
+    cout<<"Chef "<<chefName<<" luotu"<<"\n";
+};
+
+string Chef::getName(){
+    return chefName;
+};
+
+int Chef::makeSalad(int num1){
+    cout<<"Chef Salaatti";
+    int portions=num1/5;
+    return portions;
+};
+
+int Chef::makeSoup(int num1){
+    cout<<"Chef soppa";
+    int portions=num1/3;
+    return portions;
+};
