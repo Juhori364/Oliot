b7b5c2d0f984950fda2affa08ab9d22a0d817156
Juhori364 <163916599+Juhori364@users.noreply.github.com>
2025-02-12 20:07:26 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
-#include "pankkitili.H"
-#include "luottotili.h"
-#include "asiakas.h"
+#include "seuraaja.h"
+#include "notifikaattori.h"
+
 using namespace std;
 
 int main()
 {
-    asiakas testi("jonne", 1000);
-    testi.talletus(400);
-    asiakas testi2("Janne", 1000);
-    testi.tiliSiirto(300, testi2);
-    testi.showSaldo();
-    testi2.showSaldo();
+    Seuraaja *A = new Seuraaja("jaakko");
+    Seuraaja *B = new Seuraaja("joona");
+    Seuraaja *C = new Seuraaja("Jussi");
 
-};
+    Notifikaattori testi;
+    testi.lisaa(A);
+    testi.lisaa(B);
+    testi.lisaa(C);
+
+    testi.tulosta();
+
+    testi.poista(B);
+
+    testi.tulosta();
+
+    testi.postita("testi");
+
+    delete A;
+    delete B;
+    delete C;
+
+    return 0;
+}
